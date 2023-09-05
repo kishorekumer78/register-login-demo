ccc7836ef24ff731c312f36b9adf272606958250
kishorekumer78 <kishore9657@gmail.com>
2023-09-06 00:03:56 +0600

    session token name prod deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export default async function middleware(request: NextRequest) {
 	if (request.nextUrl.pathname.startsWith('/dashboard')) {
-		const sessionToken = request.cookies.get('next-auth.session-token');
+		const sessionToken = request.cookies.get('__Secure-next-auth.session-token');
 		if (sessionToken) {
+			// check valid
 			return NextResponse.next();
 		} else {
 			return NextResponse.redirect(new URL('/', request.url));
